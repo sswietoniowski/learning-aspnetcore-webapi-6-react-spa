488eb7394aead434b4765ebfb072da9718bdf173
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-09 09:11:29 +0200

    Created form component

---
+++ Spa.Frontend/src/house/HouseForm.tsx
@@ -0,0 +1,89 @@
+import { useState } from 'react';
+import { House } from '../types/house';
+
+type Args = {
+  house: House;
+  submitted: (house: House) => void;
+};
+
+const HouseForm = ({ house, submitted }: Args) => {
+  const [houseState, setHouseState] = useState({ ...house });
+
+  const onSubmit: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault();
+    submitted(houseState);
+  };
+
+  return (
+    <form className='mt-2'>
+      <div className='form-group'>
+        <label htmlFor='address'>Address</label>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Address'
+          value={houseState.address}
+          onChange={(e) =>
+            setHouseState({ ...houseState, address: e.target.value })
+          }
+        />
+      </div>
+      <div className='form-group mt-2'>
+        <label htmlFor='country'>Country</label>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Country'
+          value={houseState.country}
+          onChange={(e) =>
+            setHouseState({ ...houseState, country: e.target.value })
+          }
+        />
+      </div>
+      <div className='form-group mt-2'>
+        <label htmlFor='description'>Description</label>
+        <textarea
+          className='form-control'
+          placeholder='Description'
+          value={houseState.description}
+          onChange={(e) =>
+            setHouseState({ ...houseState, description: e.target.value })
+          }
+        />
+      </div>
+      <div className='form-group mt-2'>
+        <label htmlFor='price'>Price</label>
+        <input
+          type='number'
+          className='form-control'
+          placeholder='Price'
+          value={houseState.price}
+          onChange={(e) =>
+            setHouseState({ ...houseState, price: parseInt(e.target.value) })
+          }
+        />
+      </div>
+      <div className='form-group mt-2'>
+        <label htmlFor='image'>Image</label>
+        <input
+          id='image'
+          type='file'
+          className='form-control'
+          //   onChange={onFileSelected}
+        />
+      </div>
+      <div className='mt-2'>
+        <img src={houseState.photo}></img>
+      </div>
+      <button
+        className='btn btn-primary mt-2'
+        disabled={!houseState.address || !houseState.country}
+        onClick={onSubmit}
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default HouseForm;
