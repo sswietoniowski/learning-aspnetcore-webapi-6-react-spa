7304e6abb0d1b53ae09e69cd64f4b4dfadcd0710
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-11 12:18:26 +0200

    Looking for bugs :-)

--- Spa.Frontend/src/house/HouseList.tsx
+++ Spa.Frontend/src/house/HouseList.tsx
@@ -1,9 +1,9 @@
 import { Link, useNavigate } from 'react-router-dom';
+import { json } from 'stream/consumers';
 import ApiStatus from '../ApiStatus';
 import { currencyFormatter } from '../config';
 import useFetchHouses from '../hooks/HouseHooks';
 import useFetchUser from '../hooks/UserHooks';
-import ErrorBoundary from '../main/ErrorBoundary';
 import { House } from '../types/house';
 
 const HouseList = () => {
@@ -16,40 +16,40 @@ const HouseList = () => {
   }
 
   return (
-    <ErrorBoundary errorUI={<>Something went wrong</>}>
-      <div>
-        <div className='row mb-2'>
-          <h5 className='themeFontColor text-center'>
-            Houses currently on the market
-          </h5>
-        </div>
-        <table className='table table-hover'>
-          <thead>
-            <tr>
-              <th>Address</th>
-              <th>Country</th>
-              <th>Asking Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data &&
-              data.map((house: House) => (
-                <tr key={house.id} onClick={() => nav(`/houses/${house.id}`)}>
-                  <td>{house.address}</td>
-                  <td>{house.country}</td>
-                  <td>{currencyFormatter.format(house.price)}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-        {userClaims &&
-          userClaims.find((c) => c.type === 'role' && c.value === 'Admin') && (
-            <Link className='btn btn-primary' to='/house/add'>
-              Add
-            </Link>
-          )}
+    <div>
+      <div className='row mb-2'>
+        <h5 className='themeFontColor text-center'>
+          Houses currently on the market
+        </h5>
       </div>
-    </ErrorBoundary>
+      <table className='table table-hover'>
+        <thead>
+          <tr>
+            <th>Address</th>
+            <th>Country</th>
+            <th>Asking Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data && JSON.stringify(data)
+            // data.map((house: House) => (
+            //   <tr key={house.id} onClick={() => nav(`/houses/${house.id}`)}>
+            //     <td>{house.address}</td>
+            //     <td>{house.country}</td>
+            //     <td>{currencyFormatter.format(house.price)}</td>
+            //   </tr>
+            // ))
+          }
+        </tbody>
+      </table>
+      {userClaims &&
+        userClaims.find((c) => c.type === 'role' && c.value === 'Admin') && (
+          <Link className='btn btn-primary' to='/houses/add'>
+            Add
+          </Link>
+        )}
+    </div>
   );
 };
 
