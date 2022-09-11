b548b50759db69480bf6b0270618b7387f9f2642
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2022-09-11 12:50:15 +0200

    Reconfigured acces to the main site

--- Spa.Frontend/src/main/Header.tsx
+++ Spa.Frontend/src/main/Header.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import logo from './GloboLogo.png';
 
 type Args = {
@@ -5,10 +6,12 @@ type Args = {
 };
 
 const Header = ({ subtitle }: Args) => {
+  const nav = useNavigate();
+
   return (
     <header className='row mb-4'>
       <div className='col-5'>
-        <img src={logo} className='logo' alt='logo' />
+        <img src={logo} className='logo' alt='logo' onClick={() => nav(`/`)} />
       </div>
       <div className='col-7 mt-5 subtitle'>{subtitle}</div>
     </header>
